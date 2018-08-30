7b823bef37b8c96e39d15b7937829f5a353fae6c
Rafael Ap. Franco J. dos Santos <rafael.franco@bevicred.com.br>
2018-08-30 11:18:50 -0300

    First commit

---
+++ Modelo.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Modelo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Domain.Interfaces
+{
+    interface IRepository<T> where T : BaseEntity
+    {
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Remove(int id);
+
+        IList<T> SelectAll();
+    }
+
+}
